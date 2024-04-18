07ec45b4dd1c786639e2a0f48511dbd4733b4ba1
dhiyarisalah <18220047@std.stei.itb.ac.id>
2024-04-18 19:18:41 +0700

    update

--- train.py
+++ train.py
@@ -1,57 +1,65 @@
 import os
 import numpy as np
-import tensorflow as tf 
-import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input, Conv2D, MaxPool2D, Flatten, Dense, Dropout, GlobalAveragePooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import Callback, CSVLogger, ReduceLROnPlateau
+from sklearn.metrics import precision_score, recall_score, f1_score
 import argparse
 import json
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import load_model
-from tensorflow.keras.callbacks import Callback, CSVLogger, ReduceLROnPlateau, ModelCheckpoint
-from sklearn.metrics import precision_score, recall_score, f1_score
 
 def create_custom_model(num_classes):
-    return tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(48, 48, 1)),
-        tf.keras.layers.MaxPool2D(2, 2),
-        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-        tf.keras.layers.MaxPool2D(2, 2),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(num_classes, activation='softmax')
+    return Sequential([
+        Input(shape=(48, 48, 1)),
+        Conv2D(32, (3, 3), activation='relu'),
+        MaxPool2D(2, 2),
+        Conv2D(64, (3, 3), activation='relu'),
+        MaxPool2D(2, 2),
+        Flatten(),
+        Dense(128, activation='relu'),
+        Dropout(0.2),
+        Dense(num_classes, activation='softmax')
     ])
 
-# Adapt to use num_classes for pretrained models
 def create_vgg16_model(num_classes):
-    base_model = tf.keras.applications.VGG16(weights='imagenet', input_shape=(48, 48, 3), include_top=False)
+    base_model = tf.keras.applications.VGG16(weights='imagenet', include_top=False, input_shape=(48, 48, 3))
     base_model.trainable = False
-    return tf.keras.Sequential([
+    return Sequential([
         base_model,
-        tf.keras.layers.GlobalAveragePooling2D(),
-        tf.keras.layers.Dense(num_classes, activation='softmax')
+        GlobalAveragePooling2D(),
+        Dense(num_classes, activation='softmax')
     ])
 
 def create_resnet_model(num_classes):
-    base_model = tf.keras.applications.ResNet50(weights='imagenet', input_shape=(48, 48, 3), include_top=False)
+    base_model = tf.keras.applications.ResNet50(weights='imagenet', include_top=False, input_shape=(48, 48, 3))
     base_model.trainable = False
-    return tf.keras.Sequential([
+    return Sequential([
         base_model,
-        tf.keras.layers.GlobalAveragePooling2D(),
-        tf.keras.layers.Dense(num_classes, activation='softmax')
+        GlobalAveragePooling2D(),
+        Dense(num_classes, activation='softmax')
     ])
 
-# Custom callback for additional metrics
 class MetricsCallback(Callback):
-    def __init__(self, validation_data):
+    def __init__(self, validation_generator):
         super().__init__()
-        self.validation_data = validation_data
+        self.validation_generator = validation_generator
 
     def on_epoch_end(self, epoch, logs=None):
-        val_predict = (np.asarray(self.model.predict(self.validation_data[0]))).round()
-        val_targ = self.validation_data[1]
+        self.validation_generator.reset()
+        val_predict = []
+        val_targ = []
+
+        for x_val, y_val in self.validation_generator:
+            val_predict.extend(np.argmax(self.model.predict(x_val), axis=-1))
+            val_targ.extend(np.argmax(y_val, axis=-1))
+            if len(val_predict) >= self.validation_generator.samples:
+                break
+
         _val_precision = precision_score(val_targ, val_predict, average='macro')
         _val_recall = recall_score(val_targ, val_predict, average='macro')
         _val_f1 = f1_score(val_targ, val_predict, average='macro')
+
         logs['val_precision'] = _val_precision
         logs['val_recall'] = _val_recall
         logs['val_f1'] = _val_f1
@@ -69,33 +77,28 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
-    base_dir = '../'  
-    dataset_dir = os.path.join(base_dir, 'preprocessed_' + args.dataset)
+    dataset_dir = os.path.join('../', 'preprocessed_' + args.dataset)
 
-    train_dir = dataset_dir
-    test_dir = dataset_dir  
-
-    train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)  # Using 20% of data for validation
+    train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
     train_generator = train_datagen.flow_from_directory(
-        train_dir,
+        dataset_dir,
         target_size=(48, 48),
         batch_size=args.bs,
         class_mode='categorical',
         color_mode='rgb' if args.model in ['vgg16', 'resnet'] else 'grayscale',
-        subset='training'  
+        subset='training'
     )
 
     validation_generator = train_datagen.flow_from_directory(
-        train_dir,
+        dataset_dir,
         target_size=(48, 48),
         batch_size=args.bs,
         class_mode='categorical',
         color_mode='rgb' if args.model in ['vgg16', 'resnet'] else 'grayscale',
-        subset='validation'  
+        subset='validation'
     )
 
     num_classes = train_generator.num_classes
-
     if args.model == 'custom':
         model = create_custom_model(num_classes)
     elif args.model == 'vgg16':
@@ -109,11 +112,14 @@ def main():
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
+    # Prepare directories for saving models and logs
+    model_dir = os.path.join('../models')
+    os.makedirs(model_dir, exist_ok=True)
+
     callbacks = [
-        ModelCheckpoint(os.path.join('../models', args.fn + '.h5'), save_best_only=True),
         ReduceLROnPlateau(),
-        MetricsCallback(validation_data=(validation_generator.next()[0], validation_generator.next()[1])),
-        CSVLogger(os.path.join('../logs', args.fn + '.log'))
+        MetricsCallback(validation_generator),
+        CSVLogger(os.path.join(model_dir, args.fn + '.log'))
     ]
 
     model.fit(
@@ -122,6 +128,9 @@ def main():
         validation_data=validation_generator,
         callbacks=callbacks
     )
+
+    # Manually save the model to ensure it's in .h5 format
+    model.save(os.path.join(model_dir, args.fn + '.h5'))
 
 if __name__ == '__main__':
     main()
